88c770c85c62bdf34d20ca4e5639473fc3334297
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-07-24 12:23:57 +0100

    Login de usuário

--- src/hooks/useAutentification.jsx
+++ src/hooks/useAutentification.jsx
@@ -76,6 +76,33 @@ export const useAuthentication = () => {
         signOut(auth);
 
     }
+
+    // login - sign in
+    const login = async(data) => {
+ 
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false);
+        } catch (error) {
+            let systemErrorMessage
+
+            if(error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado."
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocurreu um erro, por favor tente mais tarde."
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+
+    }
     useEffect(() => {
         return () => setCancelled(true);
     });
@@ -86,5 +113,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
    };
 }
