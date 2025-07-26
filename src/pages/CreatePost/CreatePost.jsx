2cdd117818dedab95ceb29ff6915d8032c959080
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-07-26 19:39:50 +0100

    Inserindo dados no Firebase

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -3,6 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument"
 
 const CreatePost = () => {
 
@@ -12,8 +13,32 @@ const CreatePost = () => {
     const [tags, setTags] = useState([]);
     const [formError, setFormError] = useState("");
 
+    const {user} = useAuthValue();
+    const {insertDocument, response} = useInsertDocument("posts");
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormError("");
+
+
+        // validate image URL
+
+        // criar o array de tags
+
+        // checar todos os valores
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags,
+            uid: user.uid,
+            createBy: user.displayName
+        })
+
+        // redirect to home page
+
+
     };
 
     return (
@@ -60,7 +85,9 @@ const CreatePost = () => {
                     value={tags}
                     />
                 </label>
-                                 <button className={styles.btn}>Cadastrar</button>
+                                 {!response.loading && <button className={styles.btn}>Cadastrar</button>}
+                                 {response.loading && <button className={styles.btn} disabled>Aguarde...</button>}
+                                 {response.error && <p className={styles.error}>{response.error}</p>}
                                 
             </form>
             </div>
