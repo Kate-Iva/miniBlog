fbc0dc82cf12e7766ef179068b9e3e0a496d88e2
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-07-27 14:24:46 +0100

    Estruturando a home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,34 @@ import styles from "./Home.module.css"
 
 import React from "react";
 
-const Home = () => {
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 
+// components
+
+const Home = () => {
+const [query, setQuery] = useState("");
+const [posts] = useState([]);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
 
     return (
-      <div className={styles.Home}>
-        <h1>Home</h1>
+      <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+        <form onSubmit={handleSubmit} className={styles.searchForm}>
+          <input type="text" placeholder="Ou busque tags..." />
+          <button className={`${styles.btn} && ${styles.btnDark}`}>Pesquisar</button>
+        </form>
+        <div>Posts...</div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}> 
+          <p>Não foram encondrados posts</p>
+          <Link to="/posts/create" className={styles.btn}>Sriar primeiro post</Link>
+          </div>
+        )}
       </div>
     )
   }
