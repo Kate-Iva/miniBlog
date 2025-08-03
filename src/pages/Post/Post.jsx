ccb3f728d8e826c6ea77be0e5e0f7c3a0240c99d
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-08-03 15:49:35 +0100

    Exibindo dado de post individual

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,18 +2,26 @@ import styles from './Post.module.css';
 
 // hooks
 import { useParams } from 'react-router-dom';
+import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const Post = () => {
     const {id} = useParams();
+    const {document: post} = useFetchDocument("posts", id);
 
     return (
-
-        <div>
-            
-            <h1>Post {id} </h1>
-            
-            </div>
-    )
-}
-
-export default Post;
+        <div className={styles.postContainer}>
+          {post && (
+            <>
+              <h1>{post.title}</h1>
+              <img src={post.image} alt={post.title} />
+              <p>{post.body}</p>
+              
+              <div className={styles.tags}>
+              </div>
+            </>
+          )}
+        </div>
+      );
+    };
+    
+    export default Post;
