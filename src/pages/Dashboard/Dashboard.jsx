b6fb47141ba92990cb9542a01b2bdc70fd9ccf29
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-08-03 16:52:45 +0100

    Estruturando dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,38 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
-    return (
-        <div className={styles.dashboard}>
-            <h1 >Dashboard</h1>
-            </div>
-    )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //const { documents: posts } = useFetchDocuments("posts", null, uid);
+
+
+  const posts = [];
+  console.log(uid);
+  console.log(posts);
+
+  return (
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className={styles.btn}>Sriar primeiro post</Link>
+        </div>
+      ) : (
+        <div className={styles.post_header}>
+
+        </div>
+      )}
+
+    </div>
+  );
+};
 
 export default Dashboard;
