913674bb7e6467e016e1a5bf564fb17f60de4745
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-08-03 16:00:17 +0100

    CSS da página de post individual

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -6,20 +6,27 @@ import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const Post = () => {
     const {id} = useParams();
-    const {document: post} = useFetchDocument("posts", id);
+    const {document: post, loading} = useFetchDocument("posts", id);
 
     return (
         <div className={styles.postContainer}>
-          {post && (
-            <>
-              <h1>{post.title}</h1>
-              <img src={post.image} alt={post.title} />
-              <p>{post.body}</p>
-              
-              <div className={styles.tags}>
-              </div>
-            </>
-          )}
+            {loading && <p>Carregando post...</p>}
+            {post && (
+        <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post trata sobre:</h3>
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+              <p key={tag}>
+                <span>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
+        </>
+      )}
         </div>
       );
     };
