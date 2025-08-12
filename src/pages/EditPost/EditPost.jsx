a2729f909744f5ca9884192323074a57e56a0047
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-08-12 23:59:06 +0100

    Atualizando post

--- src/pages/EditPost/EditPost.jsx
+++ src/pages/EditPost/EditPost.jsx
@@ -3,78 +3,81 @@ import styles from "./EditPost.module.css";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 
 const EditPost = () => {
-    const { id } = useParams();
-    const { document: post } = useFetchDocument("posts", id);
+  const { id } = useParams();
+  const { document: post } = useFetchDocument("posts", id);
 
-    const [title, setTitle] = useState("");
-    const [image, setImage] = useState("");
-    const [body, setBody] = useState("");
-    const [tags, setTags] = useState([]);
-    const [formError, setFormError] = useState("");
+  console.log(post);
 
-    useEffect(() =>{
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
 
-        if(post) {
-            setTitle(post.title);
-            setBody(post.body);
-            setImage(post.image);
+  // fill form data
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setImage(post.image);
+      setBody(post.body);
 
-            const textTags = post.tagsArray.join(", ");
+      const textTags = post.tags.join(", ");
 
-            setTags(textTags);
-        }
-    }, [post]);
+      setTags(textTags);
+    }
+  }, [post]);
 
-    const {user} = useAuthValue();
-    const {insertDocument, response} = useInsertDocument("posts");
+  const { user } = useAuthValue();
 
-    const navigate =useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormError("");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
 
-        // validate image URL
+    // validate image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
 
-        try {
-            new URL(image);
-        } catch (error) {
-            setFormError("A imagem precisa ser uma URL.");
-        }
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim());
 
-        // criar o array de tags
-        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+    console.log(tagsArray);
 
-        // checar todos os valores
-        if(!title || !image || !tags || !body) {
-            setFormError("Por favor, preencha todos os campos!")
-        }
+    console.log({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+    });
 
-        if(formError) return;
-
-        insertDocument({
-            title,
-            image,
-            body,
-            tagsArray,
-            uid: user.uid,
-            createBy: user.displayName
-        })
-
-        // redirect to home page
-        navigate("/");
-
-
+    const data = {
+      title,
+      image,
+      body,
+      tags: tagsArray,
     };
 
-    return (
-        <div className={styles.editPost}>
-             {post && (
+    console.log(post);
+
+    updateDocument(id, data);
+
+    // redirect to home page
+    navigate("/dashboard");
+  };
+
+  return (
+    <div className={styles.edit_post}>
+      {post && (
         <>
           <h2>Editando post: {post.title}</h2>
           <p>Altere os dados do post como desejar</p>
@@ -101,9 +104,9 @@ const EditPost = () => {
                 value={image}
               />
             </label>
-            <p className={styles.previewTitle}>Preview da imagem atual:</p>
+            <p className={styles.preview_title}>Preview da imagem atual:</p>
             <img
-              className={styles.imagePreview}
+              className={styles.image_preview}
               src={post.image}
               alt={post.title}
             />
@@ -140,8 +143,8 @@ const EditPost = () => {
           </form>
         </>
       )}
-            </div>
-    )
-}
+    </div>
+  );
+};
 
 export default EditPost;
